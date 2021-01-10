34b716c41f88352a9e7ea96fff0e414c0726dbbd
Kevin Wu <kevinpingsydney@gmail.com>
2021-01-10 23:21:51 +1100

    added postman test requests

--- src/index.js
+++ src/index.js
@@ -10,24 +10,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  console.log(`serving / ...`);
-  res.send("Hello world! v1.2");
+  res.send("Yeah, I am feeling good.");
 });
 
 app.post("/webhook", (req, res) => {
-  console.log("** calling webhook...");
-  console.log(`webhook get called. body`, JSON.stringify(req.body));
+  console.log("*** calling webhook ...", JSON.stringify(req.body));
   let parameters = req.body.sessionInfo.parameters;
-  console.log("*** name=", parameters.name);
   let tag = req.body.fulfillmentInfo.tag;
   let jsonResponse = {};
   if (tag == CHECK_PRODUCT_AVAILABILITY) {
     let product = parameters.product.original;
     let store = parameters.store.original;
     let count = randomInt(1, 100); // dummy stock availability
-
-    console.log("**** product", product);
-    console.log("**** store", store);
     let countedProduct = pluralize(product, count, true);
     console.log("**** countedProduct", countedProduct);
 
@@ -48,12 +42,10 @@ app.post("/webhook", (req, res) => {
     };
   } else {
     jsonResponse = {
-      //fulfillment text response to be sent to the agent if there are no defined responses for the specified tag
       fulfillment_response: {
         messages: [
           {
             text: {
-              ////fulfillment text response to be sent to the agent
               text: [
                 `There are no fulfillment responses defined for "${tag}"" tag`
               ]
@@ -70,10 +62,9 @@ app.post("/webhook", (req, res) => {
 
 // start the Express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at port ${port}`);
 });
 
 function randomInt(min, max) {
-  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
